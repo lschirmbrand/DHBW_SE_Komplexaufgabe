9f8b3105f8c4dcab6ea430ec31883ace9cc3831f
lschirmbrand <luca.schirmbrand@hotmail.com>
2021-03-31 22:35:49 +0200

    Added TestSomething Scaffold, can be cont.

---
+++ src/test/java/TestRuntime.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import packageSortingCenter.PackageSortingCenter;
+
+public class TestRuntime {
+
+    @Test
+    @DisplayName("Test Something")
+    public void testSomething(){
+        PackageSortingCenter packageSortingCenter = new PackageSortingCenter();
+    }
+}
